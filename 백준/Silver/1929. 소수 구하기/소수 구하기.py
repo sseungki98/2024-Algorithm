f1a4c697fbeab217eda0749d7c3d4a417ea95a2d
Lee SeungHyun <79951703+sseungki98@users.noreply.github.com>
2024-01-01 23:41:35 +0900

    [Silver III] Title: 소수 구하기, Time: 3252 ms, Memory: 34788 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,20 +1,19 @@
 M, N = map(int, input().split())
 # 두 수 사이의 소수 출력하기
-memo = [False] * (N+1)
+store = []
 
 def arasto(n):
     end_num = int(n ** (1/2))
+    if n == 1:
+        return False
     for j in range(2, end_num+1):
         if n % j == 0:
             return False
-    memo[n] = True
+    store.append(n)
     return True
 
 
 for i in range(M, N+1):
     arasto(i)
 
-memo[1] = False
-for i in range(len(memo)):
-    if memo[i]:
-        print(i)
+print(*store)
