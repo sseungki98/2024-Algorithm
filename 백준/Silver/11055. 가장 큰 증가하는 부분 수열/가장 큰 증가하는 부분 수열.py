ad756fca73d6a5c9123dca9bc36de82146f6d259
Lee SeungHyun <79951703+sseungki98@users.noreply.github.com>
2024-01-02 16:16:49 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 232 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
@@ -1,23 +1,12 @@
 N = int(input())
 target = list(map(int, input().split()))
-dp_store = [[i] for i in target]
-dp = [1 for _ in range(N)]
-for i in range(N):
-    last_value = []
-    last_sum = 0
+dp = [item for item in target]
+for i in range(1,N):
     for j in range(i):
         if target[i] > target[j]:
-            if dp[i] < dp[j]+1:
-                dp[i] = dp[j] + 1
-                if last_sum < sum(dp_store[j]):
-                    last_value = dp_store[j]
-                    last_sum = sum(dp_store[j])
-    dp_store[i] = dp_store[i] + last_value
+            dp[i] = max(dp[i], dp[j] + target[i])
+        else:
+            dp[i] = max(dp[i], target[i])
 
-answer = 0
-for item in dp_store:
-    answer = max(answer, sum(item))
-
-
-print(answer)
+print(max(dp))
 
