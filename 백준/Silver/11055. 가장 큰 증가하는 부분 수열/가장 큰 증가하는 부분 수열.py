77e37328826f87c6fbf3287adca9e5b94dfc801c
Lee SeungHyun <79951703+sseungki98@users.noreply.github.com>
2024-01-02 16:17:13 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 176 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
@@ -5,8 +5,7 @@ for i in range(1,N):
     for j in range(i):
         if target[i] > target[j]:
             dp[i] = max(dp[i], dp[j] + target[i])
-        else:
-            dp[i] = max(dp[i], target[i])
+
 
 print(max(dp))
 
