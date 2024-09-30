55fc6fcf559ad49472214b4b6205a9d3970c3cc6
Lee SeungHyun <79951703+sseungki98@users.noreply.github.com>
2024-10-01 03:13:33 +0900

    [Silver II] Title: DFS와 BFS, Time: 492 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,45 +1,35 @@
-from collections import defaultdict
-from collections import deque
+from collections import defaultdict, deque
 N, M, V = map(int, input().split())
-route = defaultdict(list)
-bfs_route = []
-dfs_route = []
-
+graph = defaultdict(list)
 for _ in range(M):
-    start, end = map(int, input().split())
-    route[start].append(end)
-    route[end].append(start)
+    n1, n2 = map(int, input().split())
+    graph[n1].append(n2)
+    graph[n2].append(n1)
 
+dfs_visited = []
+def dfs(start_node):
+    dfs_visited.append(start_node)
+    sorted_item = sorted(graph[start_node])
+    for item in sorted_item:
+        if item not in dfs_visited:
+            dfs(item)
+            dfs_visited.append(item)
+            dfs_visited.pop()
 
-def bfs(node):
+def bfs(start_node):
+    bfs_visited = [start_node]
     dq = deque()
-    visited = []
-    dq.append(node)
-    visited.append(node)
+    dq.append(start_node)
     while dq:
-        target = dq.popleft()
-        bfs_route.append(target)
-        for item in sorted(route[target]):
-            if item not in visited:
+        node = dq.popleft()
+        for item in sorted(graph[node]):
+            if item not in bfs_visited:
                 dq.append(item)
-                visited.append(item)
+                bfs_visited.append(item)
+    return bfs_visited
 
 
-def dfs(node, visited):
-    visited.append(node)
-    dfs_route.append(node)
-    for item in sorted(route[node]):
-        if item not in visited:
-            dfs(item, visited)
-
-
-visited = []
-dfs(V, visited)
-bfs(V)
-print(*dfs_route)
-print(*bfs_route)
-
-
-
-
+dfs(V)
+print(*dfs_visited)
+print(*bfs(V))
 
